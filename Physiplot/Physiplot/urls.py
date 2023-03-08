cccd35ba98c748a4daaa5c4fd13fb374de68b955
vinayak180203 <vinayakk@iitbhilai.ac.in>
2023-03-08 23:33:45 +0530

    Added route for homepage

--- Physiplot/Physiplot/urls.py
+++ Physiplot/Physiplot/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from Physiplot import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
