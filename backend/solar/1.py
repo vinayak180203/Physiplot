03c81a2a8a592404388f71d036119c4c81652e45
sunkustar <sunkaravamsi@iitbhilai.ac.in>
2023-03-14 18:19:42 +0530

    fixed solar backend

--- backend/solar/1.py
+++ backend/solar/1.py
@@ -12,17 +12,4 @@ print(x)
 plt.plot(x,O)
 plt.show()
 
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-img = cv2.imread('12141630.png',flags = cv2.IMREAD_GRAYSCALE)
-print(img.shape)
-print(img[128])
-
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-img = cv2.imread('12141630.png',flags = cv2.IMREAD_GRAYSCALE)
-print(img.shape)
-print(img[128])
 
